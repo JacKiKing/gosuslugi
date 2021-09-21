835769baf75fd59b13a06656f16ba9f994a1bacd
JacKiKing <32509854+JacKiKing@users.noreply.github.com>
2021-09-21 23:29:46 +0300

    Add files via upload
    заменил бд, зделал таблицу, без стилей

---
+++ vue.js
@@ -0,0 +1,7 @@
+new Vue ({
+	el: '#containerTable',
+	data: {
+		name: names,
+	},
+	//template: '<div><div class="borderLine" v-for="elem in name">{{elem.ID}}<div class="borderLineRight"></div>{{ elem.Name }}</div><div v-for="elem in name"></div><br/></div>',
+});
